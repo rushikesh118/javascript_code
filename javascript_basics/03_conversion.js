340fc66d0489bb7905d2e6cda2ba1ef121ee7357
rushikesh118 <107035679+rushikesh118@users.noreply.github.com>
2023-09-23 11:19:24 +0000

    type conversion data types

---
+++ javascript_basics/03_conversion.js
@@ -0,0 +1,26 @@
+// let score ="33"
+// let score ="33abc"
+// let score =null
+// let score =undefined
+let score =false
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33"=>33
+//"33abc" => NaN
+//true => 1;false=>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 23462
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
