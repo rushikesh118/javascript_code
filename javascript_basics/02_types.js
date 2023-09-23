683b0141516ad0d782910eaa711c227532062a5c
rushikesh118 <107035679+rushikesh118@users.noreply.github.com>
2023-09-23 10:58:53 +0000

    data types

---
+++ javascript_basics/02_types.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all Js code as a newer version
+
+//alert(3+3) //gives error because we are using nodejs not browse
+
+console.log(3+3);
+console.log("rushi");
+
+//original documentation of javascript http://tc39.es/ecma262
+// mdn documendation
+
+
+// data types 
+let name = "rushi"
+let age = 18
+let isLoggedIn =false
+
+// number => 2 to power 53 or  52
+//bigint
+//string => "" prefer use double quotes use also use signle quotes ''
+
+//boolean => true/false
+
+// null => standalone value
+
+// undefined => means abhi value assign nahi huyi hai
+
+//symbol => mostly used for finding uniqueness
+
+//object
+
+// typeof used for any value type know ex:- syntax typeof "hello" or typeof(45)
+console.log(typeof isLoggedIn);
+console.log(typeof null); // output object
+console.log(typeof undefined);//output undefined
